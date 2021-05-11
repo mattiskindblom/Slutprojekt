6f155cb7f14db4a6a6fe308579167148ad24d16e
Mattis Kindblom <mattis.kindblom@elev.ga.ntig.se>
2021-05-11 11:04:37 +0200

    Jag har jobbat på hela lektionen, börjat fixa mitt tiktactoe men mitt "GameBoard" vill inte fungera atm men jag kommer nog fixa det under helgen eller under nästa lektion. Jag kommer också spendera tid på att kolla på genomgångar på nätet

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,86 @@
-/**
- * Created 2021-04-27
- *
- * @author
- */
-public class Main {
+import java.util.Random;
+import java.util.Scanner;
+
+
+class TicTacToe {
+
     public static void main(String[] args) {
-        // Här startas ditt program
+
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
+
+        printGameBoard(gameBoard);
+
+
+
+        while(true){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter your placement (1-9)");
+            int playerPos = scan.nextInt();
+
+            placePiece(gameBoard, playerPos, "player");
+
+            Random rand = new Random();
+            int cpuPos = rand.nextInt(9) +1;
+            placePiece(gameBoard, cpuPos, "cpu");
+
+            printGameBoard(gameBoard);
+        }
+
+
     }
-}
+
+    public static void printGameBoard (char[][] gameBoard) {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
+                System.out.println(c);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void placePiece(char[][] gameBoard, int pos, String user) {
+
+        char symbol = ' ';
+
+        if(user.equals("player")) {
+            symbol = 'O';
+        } else if(user.equals("cpu")) {
+            symbol = 'X';
+        }
+
+        switch (pos) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
+            default:
+                break;
+        }
+    }}
